1fbf9ac4f1f79c696c140a5ee1d31c536c234403
ryannseymour <ryannseymour3@gmail.com>
2025-02-06 17:18:31 -0500

    Login page and okta verification

--- airport-capacity-management/client/src/App.js
+++ airport-capacity-management/client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
+import oktaAuth from "./OktaConfig";
+
+import LoginPage from "./Login";
+import ProtectedPage from "./protected";
 
 function App() {
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(originalUri || "/protected");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+        <Switch>
+          {/* Default route - Show Login Page */}
+          <Route exact path="/" component={LoginPage} />
+
+          {/* Callback route - called after user is authenticated */}
+          {/* <Route exact pat  h="/login/callback" component={LoginCallback} /> */}
+          
+          {/* Protected Route example page (requires authentication) - page visited after authentication */}
+          <SecureRoute path="/protected" component={ProtectedPage} />
+
+          {/* Explicit Login Route */}
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+      </Security>
+    </Router>
   );
 }
 
---
+++ airport-capacity-management/client/src/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useOktaAuth } from "@okta/okta-react";
+import "./Login.css";
+import logo from "./resources/netjets-logo.png";
+
+function LoginPage() {
+  const { oktaAuth } = useOktaAuth();
+
+  if (!oktaAuth) {
+    return <p>Failure with Okta Auth.</p>;
+  }
+
+  const login = async () => {
+    await oktaAuth.signInWithRedirect({ originalUri: "/protected" });
+  };
+
+  return (
+    <div className="login-container">
+      <img src={logo} alt="NetJets Logo" className="logo" />
+      <button onClick={login}>Login with Okta</button>
+    </div>
+  );
+}
+
+export default LoginPage;
