95d893971044373796e133526781483872f8cdbe
emtelgen <telgen12@msu.edu>
2025-04-08 12:43:52 -0400

    changed color of points

--- airport-capacity-management/client/src/components/MapContainer.js
+++ airport-capacity-management/client/src/components/MapContainer.js
@@ -150,7 +150,7 @@ const MapContainer = ({ markers, smallMarkers, onMarkerClick, setMapInstance }) 
           title: markerData.title,
           icon: {
             path: icon,
-            fillColor: 'red',
+            fillColor: '#1c3149',
             fillOpacity: 1,
             strokeColor: "rgb(33,48,71)",
             strokeWeight: 1,
