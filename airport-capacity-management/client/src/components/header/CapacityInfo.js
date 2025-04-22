4ef8f6296bbb0c30de434c102a1b51295d866040
Jay <scottjay@msu.edu>
2025-04-22 01:26:20 -0400

    invisible characters

--- airport-capacity-management/client/src/components/header/CapacityInfo.js
+++ airport-capacity-management/client/src/components/header/CapacityInfo.js
@@ -1,27 +1,28 @@
 /**
  * Total airport Capacity and individual FBO Capacities
  * Of Simulator component
- * Displays in the Header of the simulator component
- *
+ * Displays in the Header of the simulator component 
+ * 
  * Props:
  * - currentPopulation: Number of planes currently at the airport
  * - overallCapacity: Overall capacity of the airport
  * - fbo: Array of FBO data
  */
 const CapacityInfo = ({ capacity, fboCapacity }) => {
-  return (
-    <div className="header-segment-small">
-      <div>{`Airport Capacity`}</div>
-      <div>{capacity !== null ? `${capacity}%` : "0%"}</div>
-
-      {fboCapacity != null && (
-        <>
-          <div>FBO Capacity</div>
-          <div>{`${fboCapacity}%`}</div>
-        </>
-      )}
-    </div>
-  );
-};
-
-export default CapacityInfo;
+ 
+    return (
+      <div className='header-segment-small'>
+        <div>
+          {capacity !== null ? `Airport Capacity` : "\u00A0"}
+        </div>
+        <div>{capacity !== null ? `${capacity}%` : "\u00A0"}</div>
+        <div>
+          {fboCapacity != null ? `FBO Capacity` : "\u00A0"}
+        </div>
+        <div>{fboCapacity != null ? `${fboCapacity}%` : "\u00A0"}</div> 
+      </div>
+    );
+  };
+  
+  export default CapacityInfo;
+  
